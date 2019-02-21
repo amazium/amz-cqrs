1a0db2c55443c2cde251530296c84083469929e0
Jeroen Keppens <jeroen@smartwebproducts.com>
2019-02-21 18:21:10 +0100

    small changes

--- src/Application/Command/Command.php
+++ src/Application/Command/Command.php
@@ -2,9 +2,10 @@
 
 namespace Amz\Cqrs\Application\Command;
 
+use Amz\Core\Contracts\CreatableFromArray;
 use Amz\Core\Contracts\Extractable;
 
-interface Command extends Extractable
+interface Command extends Extractable, CreatableFromArray
 {
 
 }
